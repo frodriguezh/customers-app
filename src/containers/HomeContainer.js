87f35a062b1e84e98aa4b81dc0bd429dcf01d0ad
Fernando <fernandoa.rodriguezh@gmail.com>
2022-04-24 00:15:23 -0400

    Avance 240422

---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import AppFrame from './../components/AppFrame';
+import CustomersActions from './../components/CustomersActions';
+
+class HomeContainer extends Component {
+
+    handleOnClick = () => {
+        console.log("handleOn Click");
+        this.props.history.push('/customers');
+    }
+
+    render() {
+        return (
+            <div>
+                <AppFrame
+                    header='Inicio'
+                    body={
+                        <div>
+                            <CustomersActions>
+                                <button onClick={this.handleOnClick} >Listado de clientes</button>
+                                
+                            </CustomersActions>        
+                        </div>
+                    }
+
+                    ></AppFrame>
+            </div>
+        );
+    }
+}
+
+export default withRouter(HomeContainer);

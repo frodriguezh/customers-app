9f25397ae383e68767b93d512c24c3abb93bd110
Fernando <fernandoa.rodriguezh@gmail.com>
2022-04-28 00:10:00 -0400

    Avance 280422

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
 import {setPropsAsInitial } from './../helpers/setPropsAsInitial'
@@ -23,15 +23,6 @@ const validate = values => {
     return error;
 };
 
-const MyField = ({ input, meta, type, label, name }) => (
-    <div>
-        <label htmlFor={name}>{label}</label>
-        <input {...input} type= { !type ? "text" : type} />
-        { 
-            meta.touched && meta.error && <span>{meta.error}</span> 
-        }
-    </div>
-);
 
 const toNumber = value => value && Number(value);
 const toUpper = value => value && value.toUpperCase();
@@ -40,43 +31,66 @@ const toGrow = (value, previousValue, values) =>
     value && (!previousValue ? value : (value > previousValue ? value : previousValue))
 
 
-const CustomerEdit = ({ 
-    name ,dni, age, handleSubmit, submitting, onBack, pristine, submitSucceeded }) => {
-    return (
+class CustomerEdit extends Component {
+
+    componentDidMount() {
+        if(this.txt){
+            this.txt.focus();
+        }
+    }
+    
+
+    renderField = ({ input, meta, type, label, name, withFocus }) => (
         <div>
-            <h2>Edición de cliente</h2> 
-            <form onSubmit={handleSubmit}>
-                <Field 
-                    name="name" 
-                    component={MyField} 
-                    type="text"
-                    label="Nombre:"
-                    parse={toUpper}
-                    format={toLower}></Field>
-                <Field 
-                    name="dni" 
-                    component={MyField} 
-                    type="text"
-                    validate={isNumber}
-                    label="DNI:"></Field>
-                <Field 
-                    name="age" 
-                    component={MyField}  
-                    type="number"
-                    validate={isNumber}
-                    label="Edad:"
-                    parse={toNumber}
-                    normalize={toGrow}></Field>
-                <CustomerActions>
-                    <button type="submit" disabled={pristine ||submitting}>Aceptar</button>
-                    <button type="button" onClick={onBack}>Cancelar</button>
-                </CustomerActions>
-                <Prompt when={!pristine && !submitSucceeded} 
-                    message="Se perderán los datos si continúa"></Prompt>
-            </form>
+            <label htmlFor={name}>{label}</label>
+            <input {...input} 
+                type= { !type ? "text" : type} 
+                ref= { withFocus && (txt => this.txt = txt) } />
+            { 
+                meta.touched && meta.error && <span>{meta.error}</span> 
+            }
         </div>
     );
-};
+
+    render(){
+    const { handleSubmit, submitting, onBack, pristine, submitSucceeded } = this.props;
+        return (
+            <div>
+                <h2>Edición de cliente</h2> 
+                <form onSubmit={handleSubmit}>
+                    <Field 
+                        withFocus
+                        name="name" 
+                        component={this.renderField} 
+                        type="text"
+                        label="Nombre:"
+                        parse={toUpper}
+                        format={toLower}></Field>
+                    <Field 
+                        name="dni" 
+                        component={this.renderField} 
+                        type="text"
+                        validate={isNumber}
+                        label="DNI:"></Field>
+                    <Field 
+                        name="age" 
+                        component={this.renderField}  
+                        type="number"
+                        validate={isNumber}
+                        label="Edad:"
+                        parse={toNumber}
+                        normalize={toGrow}></Field>
+                    <CustomerActions>
+                        <button type="submit" disabled={pristine ||submitting}>Aceptar</button>
+                        <button type="button" onClick={onBack}>Cancelar</button>
+                    </CustomerActions>
+                    <Prompt when={!pristine && !submitSucceeded} 
+                        message="Se perderán los datos si continúa"></Prompt>
+                </form>
+            </div>
+        )
+    }
+}
 
 CustomerEdit.propTypes = {  
     name: PropTypes.string,
--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -9,6 +9,7 @@ import CustomerEdit from './../components/CustomerEdit'
 import CustomerData from './../components/CustomerData'
 import { fetchCustomers } from './../actions/fetchCustomers'
 import { updateCustomer } from './../actions/updateCustomer'
+import { deleteCustomer } from './../actions/deleteCustomer'
 import { SubmissionError } from 'redux-form';
 
 class CustomerContainer extends Component {
@@ -20,7 +21,6 @@ class CustomerContainer extends Component {
     }
     
     handleSubmit = values => {
-        //console.log(JSON.stringify(values));
         const { id }  = values;
         return this.props.updateCustomer(id, values).then( r => {
             if (r.error) {
@@ -37,21 +37,37 @@ class CustomerContainer extends Component {
         this.props.history.goBack();
     }
 
+    handleOnDelete = (id) =>{
+        this.props.deleteCustomer(id).then( v => {
+            this.props.history.goBack();
+        });
+    }
+
+    renderCustomerControl = (isEdit, isDelete) => {
+
+        if(this.props.customer){
+            const CustomerControl = isEdit ? CustomerEdit : CustomerData;
+            return <CustomerControl {...this.props.customer} 
+                onSubmit={this.handleSubmit}
+                onSubmitSuccess={this.handleSubmitSuccess} 
+                onBack={this.handleOnBack}
+                isDeleteAllow ={!!isDelete}
+                onDelete={this.handleOnDelete} />
+
+        }
+
+        return null;
+    }
+
     renderBody = () => (
         <Route path="/customers/:dni/edit" children= {  
-            ({ match }) => {
-
-                if(this.props.customer){
-                    const CustomerControl = match ? CustomerEdit : CustomerData;
-                    return <CustomerControl {...this.props.customer} 
-                        onSubmit={this.handleSubmit}
-                        onSubmitSuccess={this.handleSubmitSuccess} 
-                        onBack={this.handleOnBack} />
-
-                }
-
-                return null;
-            }
+            ({ match: isEdit }) => (
+                <Route path="/customers/:dni/del" children= {  
+                    ({ match: isDelete }) => (
+                        this.renderCustomerControl(isEdit, isDelete)
+                    )
+                }/>
+            )
         }/>
     )
 
@@ -73,6 +89,7 @@ CustomerContainer.propTypes = {
     customer: PropTypes.object,
     fetchCustomers: PropTypes.func.isRequired,
     updateCustomer: PropTypes.func.isRequired,
+    deleteCustomer: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state, props) => ({
@@ -81,5 +98,6 @@ const mapStateToProps = (state, props) => ({
 
 export default withRouter(connect(mapStateToProps, {
     fetchCustomers,
-    updateCustomer
+    updateCustomer,
+    deleteCustomer,
 })(CustomerContainer));
